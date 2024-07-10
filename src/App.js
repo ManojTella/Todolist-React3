271d66c7290ad23abb070f3d62b61941befc1f73
ManojTella <manojtella7@gmail.com>
2024-07-10 14:13:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList'; // Ensure the relative path is correct
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+export default App;
